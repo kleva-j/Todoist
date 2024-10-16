120bc8abdf79386b248426e080a54123318d3115
kleva-j <kasmickleva@gmail.com>
2024-10-16 23:47:41 +0100

    feat: install convex and setup env variables

---
+++ env.mjs
@@ -0,0 +1,25 @@
+import { createEnv } from "@t3-oss/env-nextjs";
+import { z } from "zod";
+
+export const env = createEnv({
+  server: {
+    CONVEX_DEPLOYMENT: z.string().min(10),
+  },
+  client: {
+    NEXT_PUBLIC_CONVEX_URL: z.string().min(10),
+  },
+  runtimeEnv: {
+    CONVEX_DEPLOYMENT: process.env.CONVEX_DEPLOYMENT,
+    NEXT_PUBLIC_CONVEX_URL: process.env.NEXT_PUBLIC_CONVEX_URL,
+  },
+  /**
+   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
+   * useful for Docker builds.
+   */
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+  /**
+   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
+   * `SOME_VAR=''` will throw an error.
+   */
+  emptyStringAsUndefined: true,
+});
