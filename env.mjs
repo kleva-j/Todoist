45198b130f9abf993d2b709c7384507062da5c54
kleva-j <kasmickleva@gmail.com>
2024-10-17 22:06:30 +0100

    feat: setup jwt templates and webhook-secret for convex on clerk

--- env.mjs
+++ env.mjs
@@ -5,6 +5,8 @@ export const env = createEnv({
   server: {
     CONVEX_DEPLOYMENT: z.string().min(10),
     CLERK_SECRET_KEY: z.string().min(10),
+    CLERK_WEBHOOK_SECRET: z.string().min(1).max(50),
+    CLERK_ISSUER_URL: z.string().url("Must be a valid URL"),
   },
   client: {
     NEXT_PUBLIC_CONVEX_URL: z.string().min(10),
@@ -16,6 +18,8 @@ export const env = createEnv({
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    CLERK_WEBHOOK_SECRET: process.env.CLERK_WEBHOOK_SECRET,
+    CLERK_ISSUER_URL: process.env.CLERK_ISSUER_URL,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
