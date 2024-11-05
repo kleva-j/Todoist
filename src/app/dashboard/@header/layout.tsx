0adab609cd8a9033cbf1a97451ee247b7dc6b71b
kleva-j <kasmickleva@gmail.com>
2024-11-05 12:35:31 +0100

    feat: modify mode-toggle component props

--- src/app/dashboard/@header/layout.tsx
+++ src/app/dashboard/@header/layout.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import type { PropsWithChildren } from "react";
 
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { ModeToggle } from "@/components/mode-toggle";
 import { Separator } from "@/components/ui/separator";
 
 import {
@@ -15,7 +16,7 @@ import {
 
 function DashboardHeaderLayout({ children }: PropsWithChildren) {
   return (
-    <header className="flex sticky top-0 h-16 shrink-0 items-center gap-2 border-b bg-background">
+    <header className="flex sticky top-0 h-16 shrink-0 items-center gap-2 border-b bg-background justify-between">
       <div className="flex items-center gap-2 px-4">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 h-4" />
@@ -30,6 +31,10 @@ function DashboardHeaderLayout({ children }: PropsWithChildren) {
             {children}
           </BreadcrumbList>
         </Breadcrumb>
+      </div>
+
+      <div className="mr-4 flex gap-2 flex-wrap items-center">
+        <ModeToggle className="[&>svg]:size-3.5 [&>svg]:stroke-[1px] size-5" />
       </div>
     </header>
   );
--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -1,9 +1,10 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
+import { cn } from "@/lib/utils";
 
-import { Button } from "@/components/ui/button";
 import {
   DropdownMenuContent,
   DropdownMenuTrigger,
@@ -11,13 +12,17 @@ import {
   DropdownMenu,
 } from "@/components/ui/dropdown-menu";
 
-export function ModeToggle() {
+export interface ModeToggleProps {
+  className?: string;
+}
+
+export function ModeToggle({ className }: ModeToggleProps) {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline" size="icon" className={cn(className)}>
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
