3ffe3804cb59ee45ccb5e7069d0ee8154f507779
kleva-j <kasmickleva@gmail.com>
2024-11-05 12:38:53 +0100

    feat: modify next-image component to manage error state

---
+++ src/components/next-image.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import NextImage, { type ImageProps } from "next/image";
+
+import { placeholderImage } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+type Props = ImageProps & {
+  errorText: string;
+};
+
+export function WithErrorImage({ className, errorText, src, ...props }: Props) {
+  const [newSrc, setSrc] = useState(src);
+  const [isLoading, setLoading] = useState(true);
+  return (
+    <NextImage
+      {...props}
+      className={cn(
+        className,
+        isLoading
+          ? "scale-110 blur-2xl grayscale"
+          : "scale-100 blur-0 grayscale-0"
+      )}
+      src={newSrc}
+      onError={() => setSrc(placeholderImage(errorText))}
+      onLoad={() => setLoading(false)}
+    />
+  );
+}
